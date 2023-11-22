536622b1e2a7ab914728877e0a8870167888305c
Ankit Kaushik <ankitkaushikx@gmail.com>
2023-11-22 17:15:03 +0530

    Contact App Working Right with Different Components , Map on Contacts Array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,14 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      {contacts.map(function (item, index) {
+        return <Card key={index} name={item.name} src={item.imgURL} tel={item.phone} email={item.email} />;
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(<App />);
 
 //1. Apply CSS styles to App.jsx component
 //to match the appearance on the completed app:
